c0e742af3c1a27da0219a6a3da5f1271d35577fe
ollie-ollie-tech-deck <109923985+kirisamemofo@users.noreply.github.com>
2026-08-09 16:49:15 -0400

    Update exit_barrier.asm

--- src/anims/r3/exit_barrier.asm
+++ src/anims/r3/exit_barrier.asm
@@ -10,6 +10,7 @@
 	dc.b	0, 1, 2, 3, 4, 5, 6, 7
 	dc.b	7
 	; Instead of using a flag to denote the end of the animation, 
-	; the object manually checks for it.
+	; the object manually checks for it. Which sprite frame it
+	; checks depends on the given width.
 
 ; ------------------------------------------------------------------------------
