7b5591ec12b09d4b1c160b28dac7232103f71cd9
jamesj999 <jamesj999@gmail.com>
2026-08-20 22:09:24 +0100

    Add validated Collision Chaos block data

---
+++ src/Level/Collision Chaos/Blocks (Act 1 Present).asm
@@ -0,0 +1,7 @@
+; Collision Chaos Act 1 Present block data.
+; Origin: origin/test:src/maps/r31a/blocks.nem
+; Tracked source size: 0xF78 (3,960 bytes).
+
+LevelBlocks:
+	incbin	"Level/Collision Chaos/Data/Blocks (Act 1 Present).nem"
+	even
