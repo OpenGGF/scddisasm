aa78d1d76980098ce5f073482186e24d78c0c0e1
jamesj999 <jamesj999@gmail.com>
2026-08-20 22:11:20 +0100

    Add validated Collision Chaos map data

---
+++ src/Level/Collision Chaos/Background (Act 1 Present).asm
@@ -0,0 +1,6 @@
+; Collision Chaos Act 1 Present background map data.
+; Origin: origin/test:src/maps/r31a/background.bin
+
+StageMapBg:
+	incbin	"Level/Collision Chaos/Data/Background (Act 1 Present).bin"
+	even
---
+++ src/Level/Collision Chaos/Foreground (Act 1 Present).asm
@@ -0,0 +1,6 @@
+; Collision Chaos Act 1 Present foreground map data.
+; Origin: origin/test:src/maps/r31a/foreground.bin
+
+StageMapFg:
+	incbin	"Level/Collision Chaos/Data/Foreground (Act 1 Present).bin"
+	even
