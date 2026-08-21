e8a2a83f5d6d809867bb307cce878880dbce3c7a
jamesj999 <jamesj999@gmail.com>
2026-08-21 13:50:52 +0100

    Recover R4 main and scroll branches

--- src/r4/main.asm
+++ src/r4/main.asm
@@ -300,6 +300,8 @@ loc_20186C:
 loc_20187A:
 	move.b	#8,vblank_routine
 	bsr.w	VSync
+	cmpi.b	#6,player_object+obj.routine
+	bcc.s	loc_201898
 	tst.b	control_locked
 	bne.s	loc_201898
 	btst	#7,p1_joy_tap
@@ -314,8 +316,7 @@ loc_201898:
 	tst.b	time_attack
 	bne.s	loc_2018FC
 	andi.b	#$70,d0
-	cmpi.b	#$70,d0
-	bne.s	loc_20187A
+	beq.s	loc_20187A
 	subq.b	#1,lives
 	bpl.s	loc_2018CA
 	clr.b	lives
