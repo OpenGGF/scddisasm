98edb0a8282f3f92f95449f96797c8ebce1e19eb
jamesj999 <jamesj999@gmail.com>
2026-08-21 14:13:11 +0100

    Recover R12A palette layout

--- src/Level/Palmtree Panic/Palette Data (Act 2 Present).asm
+++ src/Level/Palmtree Panic/Palette Data (Act 2 Present).asm
@@ -44,6 +44,20 @@ Pal_Sonic:
 	incbin	"Level/_Objects/Sonic/Data/Palette.bin"
 	even
 
+; R12A level palette
+Pal_Level:
+	dcb.b	4,0
+	dc.b	8, 0, $E, $40, $E, $64, 0, 6, $E, $EE, 0, $8E, 8, $88, 4
+	dc.b	$44, $E, $8E, $A, $4E, 0, $EE, 0, $88, 0, $44, 0, $2E, $A
+	dc.b	0, 8, $EE, 8, $E, 0, 4, 0, $8E
+	dcb.b	3,0
+	dc.b	$4C, 0, $26, 2, $E8, 0, $A0, 0, $60, 0, $40, $E, $CA, $E
+	dc.b	$A8, $C, $60, $E, $86, $A, 0, $E, $EE, $E, $EA, $C, $C6, $E
+	dc.b	$64, 0, $EE, 0, $8E, 0, $6A, 0, $24
+	dcb.b	2,0
+	dc.b	$E, $EC, $E, $C8, $E, $86, 0, $42, 0, $A6, 0, $64
+	even
+
 ; R12A level palette (prototype)
 Pal_LevelProto:
 	dcb.b	4,0
@@ -61,16 +75,7 @@ Pal_LevelProto:
 	dc.b	$26, 0, $6A, 0, $AE
 	even
 
-; R12A level palette
-Pal_Level:
-	dcb.b	4,0
-	dc.b	8, 0, $E, $40, $E, $64, 0, 6, $E, $EE, 0, $8E, 8, $88, 4
-	dc.b	$44, $E, $8E, $A, $4E, 0, $EE, 0, $88, 0, $44, 0, $2E, $A
-	dc.b	0, 8, $EE, 8, $E, 0, 4, 0, $8E
-	dcb.b	3,0
-	dc.b	$4C, 0, $26, 2, $E8, 0, $A0, 0, $60, 0, $40, $E, $CA, $E
-	dc.b	$A8, $C, $60, $E, $86, $A, 0, $E, $EE, $E, $EA, $C, $C6, $E
-	dc.b	$64, 0, $EE, 0, $8E, 0, $6A, 0, $24
-	dcb.b	2,0
-	dc.b	$E, $EC, $E, $C8, $E, $86, 0, $42, 0, $A6, 0, $64
+; Level palette loaded at the end of the level after going past Amy
+Pal_LevelEnd:
+	incbin	"Level/Palmtree Panic/Data/Palette (Present).bin"
 	even
