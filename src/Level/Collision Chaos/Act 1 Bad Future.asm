64eade66bda5791f7a6d7ea3d0c571854e8ba29b
jamesj999 <jamesj999@gmail.com>
2026-08-23 03:26:48 +0100

    Make USA R31D byte-exact

--- src/Level/Collision Chaos/Act 1 Bad Future.asm
+++ src/Level/Collision Chaos/Act 1 Bad Future.asm
@@ -4,11 +4,6 @@
 ; Collision Chaos Act 1 Bad Future
 ; -------------------------------------------------------------------------
 
-	include	"Level/_Definitions.inc"
-CC_VARIANT	EQU	3
-STAGE_TIME	EQU	2
-STAGE_GOOD_FUTURE	EQU	0
-
 ; -------------------------------------------------------------------------
 
-	include	"Level/Collision Chaos/Act 1 Shared.asm"
+	include	"Level/Collision Chaos/R31D Legacy.asm"
---
+++ src/r3/stage_data_load_1d.asm
@@ -0,0 +1,78 @@
+; ------------------------------------------------------------------------------
+
+LoadStageData:
+	moveq	#0,d0
+	lea	StageDataIndex,a2
+	move.l	a2,-(sp)
+	addq.l	#4,a2
+	move.l	(a2)+,d1
+	andi.l	#$3FFFFF,d1
+	movea.l	d1,a0
+	lea	stage_blocks,a4
+	bsr.w	DecompNemesis
+	movea.l	(a2)+,a0
+	bsr.w	LoadStageMap
+	move.w	(a2)+,d0
+	move.w	(a2),d0
+	andi.w	#$FF,d0
+	bsr.w	LoadFadePalette
+	movea.l	(sp)+,a2
+	addq.w	#4,a2
+	tst.b	spawn_mode
+	beq.s	loc_203580
+	jmp	InitSectionGfx
+
+; ------------------------------------------------------------------------------
+
+loc_203580:
+	btst	#1,stage_start_flags
+	beq.s	locret_203594
+	moveq	#0,d0
+	move.b	(a2),d0
+	beq.s	locret_203594
+	bsr.w	AddGfxQueue
+
+locret_203594:
+	rts
+
+; ------------------------------------------------------------------------------
+
+LoadStageMap:
+	lea	stage_map,a3
+	move.w	#$1FF,d1
+	moveq	#0,d0
+
+loc_2035A0:
+	move.l	d0,(a3)+
+	dbf	d1,loc_2035A0
+	lea	stage_map,a3
+	moveq	#0,d1
+	bsr.w	sub_2035B6
+	lea	stage_map+$40,a3
+	moveq	#2,d1
+
+; ------------------------------------------------------------------------------
+
+sub_2035B6:
+	moveq	#0,d0
+	add.w	d1,d0
+	lea	StageMaps,a1
+	move.w	(a1,d0.w),d0
+	lea	(a1,d0.w),a1
+	moveq	#0,d1
+	move.w	d1,d2
+	move.b	(a1)+,d1
+	move.b	(a1)+,d2
+
+loc_2035D0:
+	move.w	d1,d0
+	movea.l	a3,a0
+
+loc_2035D4:
+	move.b	(a1)+,(a0)+
+	dbf	d0,loc_2035D4
+	lea	$80(a3),a3
+	dbf	d2,loc_2035D0
+	rts
+
+; ------------------------------------------------------------------------------
