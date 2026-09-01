bebb38bda16a5a77b5025591aa05da54dfaf52dc
jamesj999 <jamesj999@gmail.com>
2026-09-01 18:59:33 +0100

    Structure HUD and points mapping data

--- src/Level/_Objects/HUD and Points/Data/Mappings (HUD).asm
+++ src/Level/_Objects/HUD and Points/Data/Mappings (HUD).asm
@@ -1,165 +1,51 @@
 .Map:
-	dc.w	unk_20AD6E-.Map
-	dc.w	unk_20ADB6-.Map
-	dc.w	unk_20ADC6-.Map
-	dc.w	unk_20ADFE-.Map
-unk_20AD6E:	dc.b	 $E
-	dc.b	  0
-	dc.b	  1
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  5
-	dc.b	  0
-	dc.b	  2
-	dc.b	  8
-	dc.b	  0
-	dc.b	  1
-	dc.b	  0
-	dc.b	  6
-	dc.b	$18
-	dc.b	  0
-	dc.b	  1
-	dc.b	  0
-	dc.b	  8
-	dc.b	$20
-	dc.b	  0
-	dc.b	  9
-	dc.b	  0
-	dc.b	$1B
-	dc.b	$28 ; (
-	dc.b	  0
-	dc.b	 $D
-	dc.b	  0
-	dc.b	$21 ; !
-	dc.b	$40 ; .
-	dc.b	$10
-	dc.b	  9
-	dc.b	  0
-	dc.b	 $A
-	dc.b	  0
-	dc.b	$10
-	dc.b	  1
-	dc.b	  0
-	dc.b	  8
-	dc.b	$18
-	dc.b	$10
-	dc.b	  1
-	dc.b	  0
-	dc.b	$29 ; )
-	dc.b	$28 ; (
-	dc.b	$10
-	dc.b	  0
-	dc.b	  0
-	dc.b	$18
-	dc.b	$30 ; 0
-	dc.b	$10
-	dc.b	  5
-	dc.b	  0
-	dc.b	$2B ; +
-	dc.b	$38 ; 8
-	dc.b	$10
-	dc.b	  0
-	dc.b	  0
-	dc.b	$19
-	dc.b	$48 ; H
-	dc.b	$10
-	dc.b	  5
-	dc.b	  0
-	dc.b	$2F ; /
-	dc.b	$50 ; P
-	dc.b	$20
-	dc.b	  9
-	dc.b	  0
-	dc.b	$33 ; 3
-	dc.b	$30 ; 0
-	dc.b	  0
-unk_20ADB6:	dc.b	  3
-	dc.b	  0
-	dc.b	  5
-	dc.b	  0
-	dc.b	$39 ; 9
-	dc.b	  0
-	dc.b	  8
-	dc.b	  0
-	dc.b	  0
-	dc.b	$1A
-	dc.b	$10
-	dc.b	  4
-	dc.b	  1
-	dc.b	  0
-	dc.b	$3D ; =
-	dc.b	$18
-unk_20ADC6:	dc.b	 $B
-	dc.b	  0
-	dc.b	  1
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  5
-	dc.b	  0
-	dc.b	  2
-	dc.b	  8
-	dc.b	  0
-	dc.b	  1
-	dc.b	  0
-	dc.b	  6
-	dc.b	$18
-	dc.b	  0
-	dc.b	  1
-	dc.b	  0
-	dc.b	  8
-	dc.b	$20
-	dc.b	  0
-	dc.b	  9
-	dc.b	  0
-	dc.b	$1B
-	dc.b	$28 ; (
-	dc.b	  0
-	dc.b	  9
-	dc.b	  0
-	dc.b	$21 ; !
-	dc.b	$40 ; .
-	dc.b	$10
-	dc.b	  9
-	dc.b	  0
-	dc.b	 $A
-	dc.b	  0
-	dc.b	$10
-	dc.b	  1
-	dc.b	  0
-	dc.b	  8
-	dc.b	$18
-	dc.b	$10
-	dc.b	  5
-	dc.b	  0
-	dc.b	$2B ; +
-	dc.b	$38 ; 8
-	dc.b	$10
-	dc.b	  5
-	dc.b	  0
-	dc.b	$2F ; /
-	dc.b	$48 ; H
-	dc.b	$20
-	dc.b	  9
-	dc.b	  0
-	dc.b	$33 ; 3
-	dc.b	$30 ; 0
-unk_20ADFE:	dc.b	  3
-	dc.b	$20
-	dc.b	  1
-	dc.b	  0
-	dc.b	$10
-	dc.b	  0
-	dc.b	$20
-	dc.b	  9
-	dc.b	  0
-	dc.b	$12
-	dc.b	  8
-	dc.b	$20
-	dc.b	  1
-	dc.b	  0
-	dc.b	  0
-	dc.b	$20
+	dc.w	.Score-.Map
+	dc.w	.Lives-.Map
+	dc.w	.DebugPosition-.Map
+	dc.w	.Rings-.Map
+
+.Score:
+	dc.b	$E
+	dc.b	0, 1, 0, 0, 0
+	dc.b	0, 5, 0, 2, 8
+	dc.b	0, 1, 0, 6, $18
+	dc.b	0, 1, 0, 8, $20
+	dc.b	0, 9, 0, $1B, $28
+	dc.b	0, $D, 0, $21, $40
+	dc.b	$10, 9, 0, $A, 0
+	dc.b	$10, 1, 0, 8, $18
+	dc.b	$10, 1, 0, $29, $28
+	dc.b	$10, 0, 0, $18, $30
+	dc.b	$10, 5, 0, $2B, $38
+	dc.b	$10, 0, 0, $19, $48
+	dc.b	$10, 5, 0, $2F, $50
+	dc.b	$20, 9, 0, $33, $30
+	even
+
+.Lives:
+	dc.b	3
+	dc.b	0, 5, 0, $39, 0
+	dc.b	8, 0, 0, $1A, $10
+	dc.b	4, 1, 0, $3D, $18
+	even
+
+.DebugPosition:
+	dc.b	$B
+	dc.b	0, 1, 0, 0, 0
+	dc.b	0, 5, 0, 2, 8
+	dc.b	0, 1, 0, 6, $18
+	dc.b	0, 1, 0, 8, $20
+	dc.b	0, 9, 0, $1B, $28
+	dc.b	0, 9, 0, $21, $40
+	dc.b	$10, 9, 0, $A, 0
+	dc.b	$10, 1, 0, 8, $18
+	dc.b	$10, 5, 0, $2B, $38
+	dc.b	$10, 5, 0, $2F, $48
+	dc.b	$20, 9, 0, $33, $30
+	even
+
+.Rings:
+	dc.b	3
+	dc.b	$20, 1, 0, $10, 0
+	dc.b	$20, 9, 0, $12, 8
+	dc.b	$20, 1, 0, 0, $20
--- src/Level/_Objects/HUD and Points/Data/Mappings (Points).asm
+++ src/Level/_Objects/HUD and Points/Data/Mappings (Points).asm
@@ -1,77 +1,42 @@
 .Map:
-	dc.w	unk_20AC70-MapSpr_Points
-	dc.w	unk_20AC7C-MapSpr_Points
-	dc.w	unk_20AC88-MapSpr_Points
-	dc.w	unk_20AC94-MapSpr_Points
-	dc.w	unk_20ACA0-MapSpr_Points
-	dc.w	unk_20ACA6-MapSpr_Points
-unk_20AC70:	dc.b	  2
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	$F8 ; ø
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  3
-	dc.b	  0
-	dc.b	  0
-unk_20AC7C:	dc.b	  2
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  1
-	dc.b	$F8 ; ø
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  3
-	dc.b	  0
-	dc.b	  0
-unk_20AC88:	dc.b	  2
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  2
-	dc.b	$F8 ; ø
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  3
-	dc.b	  0
-	dc.b	  0
-unk_20AC94:	dc.b	  2
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  4
-	dc.b	$F8 ; ø
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  3
-	dc.b	  0
-	dc.b	  0
-unk_20ACA0:	dc.b	  1
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  4
-	dc.b	$FC ; ü
-unk_20ACA6:	dc.b	  3
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	$F4 ; ô
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  3
-	dc.b	$FC ; ü
-	dc.b	  0
-	dc.b	  0
-	dc.b	  0
-	dc.b	  3
-	dc.b	  4
+	dc.w	.RewardTier0-MapSpr_Points
+	dc.w	.RewardTier1-MapSpr_Points
+	dc.w	.RewardTier2-MapSpr_Points
+	dc.w	.RewardTier3-MapSpr_Points
+	dc.w	.RewardTier4-MapSpr_Points
+	dc.w	.RewardTier5-MapSpr_Points
+
+.RewardTier0:
+	dc.b	2
+	dc.b	0, 0, 0, 0, $F8
+	dc.b	0, 0, 0, 3, 0
+	even
+
+.RewardTier1:
+	dc.b	2
+	dc.b	0, 0, 0, 1, $F8
+	dc.b	0, 0, 0, 3, 0
+	even
+
+.RewardTier2:
+	dc.b	2
+	dc.b	0, 0, 0, 2, $F8
+	dc.b	0, 0, 0, 3, 0
+	even
+
+.RewardTier3:
+	dc.b	2
+	dc.b	0, 0, 0, 4, $F8
+	dc.b	0, 0, 0, 3, 0
+	even
+
+.RewardTier4:
+	dc.b	1
+	dc.b	0, 0, 0, 4, $FC
+	even
+
+.RewardTier5:
+	dc.b	3
+	dc.b	0, 0, 0, 0, $F4
+	dc.b	0, 0, 0, 3, $FC
+	dc.b	0, 0, 0, 3, 4
