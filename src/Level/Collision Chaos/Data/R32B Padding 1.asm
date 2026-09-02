0b119768a7ffb395f60c1e8ff0324e2b93085f60
jamesj999 <jamesj999@gmail.com>
2026-09-02 22:31:13 +0100

    Structure R32B good future main PLC

--- src/Level/Collision Chaos/Data/R32B Padding 1.asm
+++ src/Level/Collision Chaos/Data/R32B Padding 1.asm
@@ -78,7 +78,8 @@
 ; +$0E0A-+$0E19 retained historical Act 3 Good Future stage descriptor
 ; +$0E1A-+$0E3F retained historical Act 3 Good Future PLC offset table
 ; +$0E40-+$0E4D retained historical Act 3 Good Future Stage PLC
-; +$0E4E onward  retained stage/PLC bodies still to be structured
+; +$0E4E-+$0E8B retained historical Act 3 Good Future Main PLC
+; +$0E8C onward  retained stage/PLC bodies still to be structured
 ; ------------------------------------------------------------------------------
 
 ; The count, first complete piece, and first two bytes of piece 2 precede this
@@ -1425,23 +1426,32 @@ R32BRetainedPalmtreeAct3GoodFutureStagePLC:
 	dc.w	0			; VRAM destination
 	dc.l	$0023C4FA
 	dc.w	$4A80			; VRAM destination
-	dc.b	0
+
+; Ten-entry historical Act 3 Good Future Main PLC.
 R32BRetainedPalmtreeAct3GoodFutureMainPLC:
-	dc.b	9, 0, $23, $D, $A2, $6C
-	dcb.b	2,0
-	dc.b	$23, $39, $E4, $7A
-	dcb.b	2,0
-	dc.b	$23, $1E, $2E, $9E, $40, 0, $23, $21, $3A, $A4
-	dcb.b	2,0
-	dc.b	$23, $34, $BC, $AD
-	dcb.b	2,0
-	dcb.b	2,$23
-	dc.b	$5C, $B5
-	dcb.b	2,0
-	dc.b	$23, $29, $60, $D0
-	dcb.b	2,0
-	dc.b	$22, $ED, $E6, $D8, $C0, 0, $23, $37, $32, $DA, $E0, 0, $23
-	dc.b	$2E, $48, $F5, $C0, 0, 3, 0, $23, $3B, $2E, $57, $80, 0, $23
+	dc.w	9			; entry count minus one
+	dc.l	$00230DA2
+	dc.w	$6C00
+	dc.l	$002339E4
+	dc.w	$7A00
+	dc.l	$00231E2E
+	dc.w	$9E40
+	dc.l	$0023213A
+	dc.w	$A400
+	dc.l	$002334BC
+	dc.w	$AD00
+	dc.l	$0023235C
+	dc.w	$B500
+	dc.l	$00232960
+	dc.w	$D000
+	dc.l	$0022EDE6
+	dc.w	$D8C0
+	dc.l	$00233732
+	dc.w	$DAE0
+	dc.l	$00232E48
+	dc.w	$F5C0
+
+	dc.b	0, 3, 0, $23, $3B, $2E, $57, $80, 0, $23
 	dc.b	$54, $50, $6B, $60, 0, $23, $D3, $92, $6F
 	dcb.b	2,0
 	dc.b	$23, $E0, $4C, $74
